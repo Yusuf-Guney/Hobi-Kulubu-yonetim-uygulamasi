5e807510baaf624c2a6b02da178afc9a53662ad6
Yusuf-Guney <161216398+Yusuf-Guney@users.noreply.github.com>
2024-05-27 16:48:24 +0300

    Update Tabloya_Veri_Ekleme.sql

--- Tabloya_Veri_Ekleme.sql
+++ Tabloya_Veri_Ekleme.sql
@@ -1,13 +1,14 @@
+CREATE DATABASE kullanici_kayit;
 
-USE kullanici_kayit; 
+USE kullanici_kayit;
 
 CREATE TABLE USERS (
-    User_ID INT AUTO_INCREMENT PRIMARY KEY, 
-    User_Name VARCHAR(50) NOT NULL UNIQUE, 
-    E_Mail VARCHAR(100) NOT NULL UNIQUE, 
-    First_Name VARCHAR(50) NOT NULL, 
+    User_ID INT AUTO_INCREMENT PRIMARY KEY,
+    User_Name VARCHAR(50) NOT NULL UNIQUE,
+    E_Mail VARCHAR(100) NOT NULL UNIQUE,
+    First_Name VARCHAR(50) NOT NULL,
     Last_Name VARCHAR(50) NOT NULL,
-    GSM_No VARCHAR(15) NOT NULL, 
-    Birth_Date DATE NOT NULL, 
-    Password VARCHAR(255) NOT NULL 
+    GSM_No VARCHAR(15) NOT NULL,
+    Birth_Date DATE NOT NULL,
+    Password VARCHAR(255) NOT NULL
 );
