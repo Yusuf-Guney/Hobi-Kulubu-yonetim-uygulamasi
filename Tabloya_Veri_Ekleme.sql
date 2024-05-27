7a6ece3d72832efb64bc3dbcaf142b1a46eb634e
Yusuf-Guney <161216398+Yusuf-Guney@users.noreply.github.com>
2024-05-27 11:53:50 +0300

    Update Tabloya_Veri_Ekleme.sql

--- Tabloya_Veri_Ekleme.sql
+++ Tabloya_Veri_Ekleme.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE kullanici_kayit; // Veritabanını oluşturur
+CREATE DATABASE kullanici_kayit; 
 
-USE kullanici_kayit; // Veritabanını seçer
+USE kullanici_kayit; 
 
 CREATE TABLE USERS (
-    User_ID INT AUTO_INCREMENT PRIMARY KEY, // Kullanıcı ID'si (otomatik artan)
-    User_Name VARCHAR(50) NOT NULL UNIQUE, // Kullanıcı adı (benzersiz)
-    E_Mail VARCHAR(100) NOT NULL UNIQUE, // E-posta (benzersiz)
-    First_Name VARCHAR(50) NOT NULL, // İsim
-    Last_Name VARCHAR(50) NOT NULL, // Soyisim
-    GSM_No VARCHAR(15) NOT NULL, // GSM numarası
-    Birth_Date DATE NOT NULL, // Doğum tarihi
-    Password VARCHAR(255) NOT NULL // Şifre (hashlenmiş)
+    User_ID INT AUTO_INCREMENT PRIMARY KEY, 
+    User_Name VARCHAR(50) NOT NULL UNIQUE, 
+    E_Mail VARCHAR(100) NOT NULL UNIQUE, 
+    First_Name VARCHAR(50) NOT NULL, 
+    Last_Name VARCHAR(50) NOT NULL,
+    GSM_No VARCHAR(15) NOT NULL, 
+    Birth_Date DATE NOT NULL, 
+    Password VARCHAR(255) NOT NULL 
 );
